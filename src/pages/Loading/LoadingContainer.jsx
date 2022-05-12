102054aa122393ec428c29a4f4ab23f7582c15be
YERIEL-RYU <yeriel.ryu@gmail.com>
2022-05-12 16:46:10 +0900

    Loading Component 생성

---
+++ src/pages/Loading/LoadingContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import LoadingPresenter from './LoadingPresenter';
+
+const LoadingContainer = () => {
+  return (
+    <LoadingPresenter />
+  );
+};
+
+export default LoadingContainer;
---
+++ src/pages/Loading/LoadingPresenter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingPresenter = () => {
+  return (
+    <div>
+      Loading page
+    </div>
+  );
+};
+
+export default LoadingPresenter;
